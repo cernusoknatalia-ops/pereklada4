b75efb5d28008f53fe361342e2519400a115468a
Samost-back <utubafanat7@gmail.com>
2025-10-02 22:27:53 +0300

    deb

--- TRANS-4/src/components/Home.js
+++ TRANS-4/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
+alert("Якщо ти Вітя, вийди звідси!")
 function Home() {
   return (
     <div className="Home">
